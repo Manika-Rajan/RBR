a213890fb3d04dcefa27afffbbc3a7c2b6c0ea4f
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2024-12-25 11:40:53 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
-import Amplify from 'aws-amplify';
+//import Amplify from 'aws-amplify';
 //import { configure } from 'aws-amplify';
 import awsmobile from './aws-exports';
 
 // Configure Amplify.
-Amplify.configure(awsmobile);
+//Amplify.configure(awsmobile);
 //configure(awsmobile);
 
 
