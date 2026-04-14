5524170eee8b6d8b1f5e84b39a84fe20d758c831
Manika-Rajan <105493699+Manika-Rajan@users.noreply.github.com>
2026-04-14 10:21:53 +0530

    Update Reports.jsx

--- frontend/src/components/Reports.jsx
+++ frontend/src/components/Reports.jsx
@@ -6,7 +6,23 @@ const Reports = () => {
   return (
     <>
       <Navbar reports />
-      <ReportsMobile />
+
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "520px",
+          }}
+        >
+          <ReportsMobile />
+        </div>
+      </div>
     </>
   );
 };
